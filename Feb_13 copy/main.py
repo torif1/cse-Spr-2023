9d52bc7ce1e7220c1d0da9216cdd5531211738f5
torif1 <122637128+torif1@users.noreply.github.com>
2023-02-13 21:12:05 +0000

    Made formating nicer

--- Feb_13 copy/main.py
+++ Feb_13 copy/main.py
@@ -1,10 +1,8 @@
-import resteraunt2
+"""Main place to test classes from resteraunt2"""
 from resteraunt2 import Resteraunt
-from resteraunt2 import *
 
 Anthonies=Resteraunt('Anthonies','Shell Fish')
 Anthonies.set_number_served(3)
 print(Anthonies.customersserved)
 Anthonies.incrament_number_served(5)
 print(Anthonies.customersserved)
-
--- Feb_13 copy/resteraunt2.py
+++ Feb_13 copy/resteraunt2.py
@@ -1,10 +1,12 @@
+"""Module with class defenitions"""
+
 class Resteraunt:
     def __init__(self, resteraunt_name, cuisine_type):
         self.name=resteraunt_name
         self.foodtype=cuisine_type
         self.open='False'
         self.customersserved=0
-    
+
     def describe_resteraunt(self):
         print(f'foodtype={self.foodtype} and name={self.name}')
 
@@ -18,7 +20,7 @@ class Resteraunt:
             self.customersserved+=int(guests_served)
         else:
             print('Must update by a positive integer')
-    
+
     #Defining method to incament guests served by input value
     def incrament_number_served(self,incrament_value):
         incrament_value: int>0
@@ -28,7 +30,7 @@ class User:
     def __init__(self,first_name,last_name):
         self.firstname=first_name
         self.lastname=last_name
-    
+
     def describe_user(self):
         print(f"first name={self.firstname} last name={self.lastname}")
 
