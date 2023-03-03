7301026fa9b1420e8a05420f99e30435f5af35e5
torif1 <122637128+torif1@users.noreply.github.com>
2023-03-03 20:45:37 +0000

    Formated Document

--- Mar_3/main.py
+++ Mar_3/main.py
@@ -1,12 +1,12 @@
 while True:
-    num1=input('What is your first number?')
-    num2=input('What is your second number?')
+    num1 = input('What is your first number?')
+    num2 = input('What is your second number?')
     try:
-        num_sum=int(num1)+int(num2)
+        num_sum = int(num1)+int(num2)
     except:
         print("Sorry, one of the numbers you entered is in fact not a number!")
     else:
         print(f'sum:{num_sum}')
-    again=input('Would you like to play again?(y/n)')
-    if again !='y':
+    again = input('Would you like to play again?(y/n)')
+    if again != 'y':
         break
